bffcf8a24249efdcbc2aeff41247a65beb77d53c
DeraCodings <otuonyechidera@yahoo.com>
2024-11-11 22:17:03 +0100

    deleted the page.js from the Home page folder

--- src/app/Home page/HeroSection.tsx
+++ src/app/Home page/HeroSection.tsx
@@ -11,7 +11,7 @@ function HeroSection() {
   return (
     <div className="h-screen bg-cover flex justify-center items-center bg-hero-image">
       <div className="">
-        <Text />
+        {/* <Text /> */}
       </div>
     </div>
   );
